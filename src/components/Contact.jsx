8088e798c880958684381dd92a3b2cb6dd606571
Deepanshu <113329037+Kirito04@users.noreply.github.com>
2023-08-09 17:20:39 +0530

    Contact donw

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -41,9 +41,9 @@ export function ContactInfo() {
     <div className="flex flex-row items-center justify-evenly px-[2vw] py-[0.5vw] min-h-[40vw] gap-[2vw]">
       <div className="min-w-[40vw] min-h-[10vw] bg-[#ffffff] rounded-[2vw] md:rounded-3xl px-[0.5vw] py-[0.5vw] shadow-2xl">
         <div className="flex flex-col items-center justify-center">
-          <div className="flex flex-row items-center justify-between w-full">
+          <div className=" max-w-[35vw] max-auto flex flex-row items-center justify-between w-full">
             <div className="flex flex-col items-start justify-center">
-              <p className="text-[1.8vw] font-bold text-[#793300]">
+              <p className="text-[3.2vw] font-bold text-[#793300]">
                 Get in Touch
               </p>
               <hr className="w-[6vw] h-[0.5vw] bg-[#E25000]" />
@@ -55,24 +55,26 @@ export function ContactInfo() {
               <img
                 src={NewsLetter}
                 alt="NewsLetter"
-                className="-mt-[1vw] w-[25vw] "
+                className="mt-[2vw] w-[10vw] "
               />
             </div>
           </div>
-          <form className="w-full max-w-xs flex flex-col gap-8">
+          <form className="w-full max-w-[35vw] flex flex-col gap-[1vw]">
             <label className="flex flex-col">
-              <span className="text-[#793300] font-medium mb-4">Your Name</span>
+              <span className="text-[#793300] text-[1.2vw] font-medium mb-[1vw]">
+                Your Name
+              </span>
               <input
                 type="text"
                 name="name"
                 // onChange={handleChange}
                 // value={form.name}
                 placeholder="what's your name?"
-                className="bg-[#FFE7D5] shadow-md shadow-primary py-4 px-6 placeholder:text-[#E25000] text-white border-none outline-none font-medium rounded-xl"
+                className="bg-[#FFE7D5] shadow-md text-[1.2vw] shadow-primary py-[1vw] px-[2vw] placeholder:text-[#E25000] text-white border-none outline-none font-medium rounded-[1vw]"
               />
             </label>
             <label className="flex flex-col">
-              <span className="text-[#793300] font-medium mb-4">
+              <span className="text-[#793300] text-[1.2vw] font-medium mb-[1vw]">
                 Your Email
               </span>
               <input
@@ -81,53 +83,53 @@ export function ContactInfo() {
                 // onChange={handleChange}
                 // value={form.email}
                 placeholder="what's your email?"
-                className="bg-[#FFE7D5] shadow-md shadow-primary py-4 px-6 placeholder:text-[#E25000] text-white border-none outline-none font-medium rounded-xl"
+                className="bg-[#FFE7D5] shadow-md text-[1.2vw] shadow-primary py-[1vw] px-[2vw] placeholder:text-[#E25000] text-white border-none outline-none font-medium rounded-[1vw]"
               />
             </label>
             <label className="flex flex-col">
-              <span className="text-[#793300] font-medium mb-4">
+              <span className="text-[#793300] text-[1.2vw] font-medium mb-[1vw]">
                 Your Message
               </span>
               <textarea
-                rows={7}
+                rows={4}
                 name="message"
                 // onChange={handleChange}
                 // value={form.message}
                 placeholder="what do you want to say?"
-                className="bg-[#FFE7D5] shadow-md shadow-primary py-4 px-6 placeholder:text-[#E25000] text-white border-none outline-none font-medium rounded-xl"
+                className="bg-[#FFE7D5] shadow-md text-[1.2vw] shadow-primary py-[1vw] px-[2vw] placeholder:text-[#E25000] text-white border-none outline-none font-medium rounded-[1vw]"
               />
             </label>
-            <button className=" bg-gradient-to-b from-[#FE9345] via-[#FD661A] mt-7 to-[#FF611D] rounded-full w-full h-[5vh] text-white text-[2vw] lg:text-[0.8vw]">
+            <button className=" bg-gradient-to-b from-[#FE9345] via-[#FD661A] mt-[2vw] to-[#FF611D] rounded-full w-full h-[3vw] text-white text-[0.8vw]">
               REGISTER
             </button>
           </form>
         </div>
       </div>
-      <div className="flex flex-col items-start justify-center gap-8">
-        <p className="text-[2.5vw] lg:text-[2vw] font-bold text-[#793300] ">
+      <div className="flex flex-col items-start justify-center gap-[2vw]">
+        <p className="text-[2vw] font-bold text-[#793300] ">
           {'have Questions?'.toUpperCase()}
         </p>
-        <p className="text-[2.2vw] lg:text-[1.2vw] max-w-md text-[#E25000]">
+        <p className="text-[1.2vw] max-w-[20vw] text-[#E25000]">
           {"Do not hesitate to send us an email if you can't find what you're looking for.".toUpperCase()}
         </p>
-        <div className="flex flex-row items-center justify-between gap-8">
-          <img src={Email} alt="Email" className="w-[10vh] aspect-square" />
+        <div className="flex flex-row items-center justify-between gap-[2vw]">
+          <img src={Email} alt="Email" className="w-[5vw] aspect-square" />
           <div className="flex flex-col items-start justify-between">
-            <p className="text-[2vh] lg:text-[1.8vw] font-bold text-[#793300]">
+            <p className="text-[1.8vw] font-bold text-[#793300]">
               EMAIL US
             </p>
-            <p className="text-[2vh] lg:text-[1.8vw] text-[#E25000]">
+            <p className="text-[1.8vw] text-[#E25000]">
               MNL9@gmail.com
             </p>
           </div>
         </div>
-        <div className="flex flex-row items-center justify-between gap-8">
-          <img src={Call} alt="Email" className="w-[10vh] aspect-square" />
+        <div className="flex flex-row items-center justify-between gap-[2vw]">
+          <img src={Call} alt="Email" className="w-[5vw] aspect-square" />
           <div className="flex flex-col items-start justify-between">
-            <p className="text-[2vh] lg:text-[1.8vw] font-bold text-[#793300]">
+            <p className="text-[1.8vw] font-bold text-[#793300]">
               CALL US
             </p>
-            <p className="text-[2vh] lg:text-[1.8vw] text-[#E25000]">
+            <p className="text-[1.8vw] text-[#E25000]">
               xxxxxxx.xxxxxxxxx
             </p>
           </div>
@@ -139,26 +141,26 @@ export function ContactInfo() {
 
 export function FAQ() {
   return (
-    <div className="flex flex-col items-center justify-center w-full max-w-[70vw] mx-auto mb-[60vh] gap-3 lg:gap-8">
-      <div className="flex flex-row items-center justify-start w-full bg-[#FFE7D5] px-[2vw] py-[1vw] rounded-xl shadow-xl">
+    <div className="flex flex-col items-center justify-center w-full max-w-[70vw] mx-auto mb-[30vw] gap-[3vw]">
+      <div className="flex flex-row items-center justify-start w-full bg-[#FFE7D5] px-[2vw] py-[1vw] rounded-[1vw] shadow-xl">
         <p className="text-[2.5vw] lg:text-[2vw] font-bold text-[#E25000]">
           {'Questions Asked'.toUpperCase()}
         </p>
       </div>
-      <div className="flex flex-col items-center justify-center w-full bg-[#FFE7D5] px-[2vw] py-[1vw] rounded-xl shadow-xl pb-[2vh] gap-5">
+      <div className="flex flex-col items-center justify-center w-full bg-[#FFE7D5] px-[2vw] py-[1vw] rounded-[1vw] shadow-xl pb-[1vw] gap-[2vw]">
         <div className="flex flex-row items-center justify-between w-full max-w-[70vw] mx-auto">
           <div className="flex flex-col items-start justify-center">
-            <p className="text-[2.5vw] lg:text-[2vw] text-[#793300]">
+            <p className="text-[2vw] text-[#793300]">
               {'ABOUT MNL9'.toUpperCase()}
             </p>
-            <img src={ContactDiv2} alt="Logo" className="w-[15vw] " />
+            <img src={ContactDiv2} alt="Logo" className="w-[25vw] " />
           </div>
           <div>
             <img src={ContactDiv3} alt="ContactDiv3" className="w-[15vw]" />
           </div>
         </div>
         <div className="w-full max-w-[70vw] mx-auto">
-          <p className="text-[2.2vw] lg:text-[1.2vw] text-[#000000]">
+          <p className="text-[1.2vw] text-[#000000]">
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum is that it has a more-or-less normal
@@ -172,16 +174,16 @@ export function FAQ() {
           </p>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center w-full bg-[#FFE7D5] px-[2vw] py-[1vw] rounded-xl shadow-xl pb-[2vh] gap-5">
+      <div className="flex flex-col items-center justify-center w-full bg-[#FFE7D5] px-[2vw] py-[1vw] rounded-[1vw] shadow-xl pb-[1vw] gap-[2vw]">
         <div className="flex flex-row items-center justify-between w-full max-w-[70vw] mx-auto">
           <div className="flex flex-col items-start justify-center">
-            <p className="text-[2.5vw] lg:text-[2vw] text-[#793300]">
+            <p className="text-[2vw] text-[#793300]">
               {'BAnking And Payment Method'.toUpperCase()}
             </p>
           </div>
         </div>
         <div className="w-full max-w-[70vw] mx-auto">
-          <p className="text-[2.2vw] lg:text-[1.2vw] text-[#000000]">
+          <p className="text-[1.2vw] text-[#000000]">
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum is that it has a more-or-less normal
@@ -201,18 +203,18 @@ export function FAQ() {
           <img
             src={ContactDiv5}
             alt="ContactDiv5"
-            className="w-[55vw] pt-[5vh]"
+            className="w-[55vw] pt-[2vw]"
           />
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center w-full bg-[#FFE7D5] px-[2vw] py-[1vw] rounded-xl shadow-xl pb-[2vh] gap-5">
+      <div className="flex flex-col items-center justify-center w-full bg-[#FFE7D5] px-[2vw] py-[1vw] rounded-[1vw] shadow-xl pb-[1vw] gap-[2vw]">
         <div className="flex flex-row items-center justify-between w-full max-w-[70vw] mx-auto">
-          <div className="flex flex-col items-start justify-center gap-5">
-            <p className="text-[2.5vw] lg:text-[2vw] text-[#793300]">
+          <div className="flex flex-col items-start justify-center gap-[2vw]">
+            <p className="text-[2vw] text-[#793300]">
               {'Security '.toUpperCase()}
             </p>
             <div className="w-full max-w-[40vw] mx-auto">
-              <p className="text-[2.2vw] lg:text-[1.2vw] text-[#000000]">
+              <p className="text-[1.2vw] text-[#000000]">
                 It is a long-established fact that a reader will be distracted
                 by the readable content of a page when looking at its layout.
                 The point of using Lorem Ipsum is that it has a more-or-less
@@ -237,7 +239,7 @@ export function FAQ() {
 
 function Contact() {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-[3vw]">
       <Main />
       <ContactInfo />
       <div className="w-full max-w-[70vw] mx-auto">
